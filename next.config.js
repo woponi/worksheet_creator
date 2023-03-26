36a47f5062a6deba3c176d728c26ef925c9c7d59
pong <woponi@gmail.com>
2023-03-26 23:58:22 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,28 @@
 /** @type {import('next').NextConfig} */
 
-const repo = 'worksheet_creator'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: assetPrefix,
-  basePath: basePath
+  basePath: basePath,
+  // No image optimization in for export
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
