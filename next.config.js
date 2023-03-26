d93b72d11daa32272fe9743d3925479186a89e1d
pong <woponi@gmail.com>
2023-03-26 23:14:30 +0800

    init config for github page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'worksheet_creator'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath
 }
 
 module.exports = nextConfig
