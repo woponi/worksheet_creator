adc321903268eec130cfb3ad40d87638b40645ab
pong <woponi@gmail.com>
2023-03-31 18:21:22 +0800

    no longer need analytics.ts

--- utils/analytics.ts
+++
@@ -1,6 +0,0 @@
-import ReactGA from 'react-ga';
-
-export const initGA = ():void => {
-  ReactGA.initialize('G-MQ19L50V11'); // Replace with your GA tracking ID
-  console.log("GA initialized");
-};
